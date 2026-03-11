ae07236d5460606555dfdde6a746cf613bebc52e
Diego <dsbenavides.3634@unicesmag.edu,co>
2026-03-11 08:48:37 -0500

    database configuration file: local conn

---
+++ config/Example.php
@@ -0,0 +1,20 @@
+<?php
+    // This is a line comment 
+    /*
+    This is block comment
+    Developer : Diego S. Delgado
+    */
+    
+    echo"Hello word im here right now";
+    $num1 = 20;
+    $num2 = 10;
+    $add = $num1 + $num2;
+    echo "<br>";
+    echo "<font color = 'GREEN'><b>The addition is:</b></font>" . $add;
+    if ($num1 > $num2){
+        echo "<br>You win";
+    }else{
+        echo "<br>You lose";
+    }
+
+?>
