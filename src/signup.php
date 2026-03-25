5198cbfa7b87f51ef83ed707e3d2a9d29da41f34
Diego <dsbenavides.3634@unicesmag.edu,co>
2026-03-25 08:51:36 -0500

    25/03/26

--- src/signup.php
+++ src/signup.php
@@ -1,16 +1,19 @@
 <?php
 //Conceta a la base de datos
 include ('../config/database.php');
+
 //Get data
 $f_name = $_POST['fname'];
 $l_name = $_POST['lname'];
 $e_mail = $_POST ['email'];
 $m_phone = $_POST ['mphone'];
 $p_sswd = $_POST ['passwd'];
-//Query to intert int SQL
+$enc_pass = md5($p_sswd);
 
-$sql = "INSERT INTO users (firstname,lastname,email,mobile_phone,password)  values ('$f_name','$l_name','$e_mail','$m_phone','$p_sswd')";
+//Query to intert int SQL
+$sql = "INSERT INTO users (firstname,lastname,email,mobile_phone,password)  values ('$f_name','$l_name',
+'$e_mail','$m_phone','$enc_pass')";
 
 //Execute query
-pg_query($sql);
+    pg_query($sql);
 ?>
