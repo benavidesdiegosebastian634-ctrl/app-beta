26fec43227827834055edff69fe35c52a638edba
Diego <dsbenavides.3634@unicesmag.edu,co>
2026-03-27 12:15:42 -0500

    ramas

--- src/signup.php
+++ src/signup.php
@@ -1,19 +1,32 @@
-<?php
-//Conceta a la base de datos
-include ('../config/database.php');
-
-//Get data
-$f_name = $_POST['fname'];
-$l_name = $_POST['lname'];
-$e_mail = $_POST ['email'];
-$m_phone = $_POST ['mphone'];
-$p_sswd = $_POST ['passwd'];
-$enc_pass = md5($p_sswd);
-
-//Query to intert int SQL
-$sql = "INSERT INTO users (firstname,lastname,email,mobile_phone,password)  values ('$f_name','$l_name',
-'$e_mail','$m_phone','$enc_pass')";
-
-//Execute query
-    pg_query($sql);
 ?>
+<?php 
+//CONECTARSE A LA BASE DE DATOS 
+include('../config/database.php');
+
+//get data
+$f_name     =   $_POST['fname' ]  ;
+$l_name     =   $_POST['lname' ]  ;
+$e_mail     =   $_POST['email' ]  ;
+$m_phone    =   $_POST['mphone']  ;
+$p_sswd     =   $_POST['psswd']  ;
+$enc_pass   = md5($p_sswd);
+
+//query to insert into SQL
+$sql ="INSERT INTO users(firstname, lastname, email, mobile_phone, password) 
+    values('$f_name', '$l_name', '$e_mail','$m_phone','$enc_pass')";
+//execute query
+pg_query($sql);
+
+$checkEmail = "SELECT * FROM USERS WHERE email = '$e_mail'";
+$resultEmail = pg_query($local_conn, $checkEmail);
+
+if (pg_num_rows($resultEmail) > 0) {
+    echo "Error: el email ya está registrado";
+    exit;
+}
+
+
+?>
+
+
+
